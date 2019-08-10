a65036be8b80ef485fe4024a01de72fb7fbd0abb
NeMo <friend-opp_2010@hotmail.com>
2019-08-10 12:02:55 +0700

    API pokemon

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// class pokemon{
+//     constructor(name,type){
+//         this.name = name
+//         this.type = type
+//     }
+
+//     echo(){
+//         console.log(`Type of ${this.name} is ${this.type}`);
+//     }
+// }
+// Dratini = new pokemon('Dratini','Dragon')
+// Rattana = new pokemon('Rattana','Normal')
+// Eevee = new pokemon('Eevee','Normal')
+
+let pokemon = [{
+    name: 'Dratini',
+    type: 'Dragon'
+},
+{
+    name: 'Rattana',
+    type: 'Normal'
+},
+{
+    name: 'Eevee',
+    type: 'Normal'
+}]
+app.use(express.json())
+app.get('/pokemons', (req, res) => res.send(pokemon))
+
+app.post('/pokemons', (req, res) => {
+    pokemon.push(req.body)
+    res.sendStatus(201)
+})
+
+app.listen(port, () => console.log(`Pokemon API listening on port ${port}`))
